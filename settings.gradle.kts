71704c68651db6522210c7c39d70d15f8fb56c7f
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2020-10-04 01:14:00 -0400

    add note regarding plugin development and dummy *.gradle.kts file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,14 @@ file("plugins").list().forEach {
     include(":$it")
 }
 
+/**
+ * To make plugin hotswapping work during development, each plugin's project
+ * directory must contain a build file of the name $name.gradle.kts because
+ * the client's ExternalPluginFilter uses it to discover valid plugin project
+ * directories.
+ *
+ * See https://github.com/open-osrs/runelite/blob/e967884e48d9c0c78f9595105281f37c0fa793c9/runelite-client/src/main/java/net/runelite/client/plugins/ExternalPluginFileFilter.java#L24-L26.
+ */
 rootProject.children.forEach {
     it.apply {
         projectDir = file("plugins/$name")
